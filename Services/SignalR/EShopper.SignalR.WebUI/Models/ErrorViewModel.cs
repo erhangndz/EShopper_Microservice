e964d31bc31df5de1672b8355681ec82e293e933
Erhan Gündüz <erhangndz@live.com>
2024-08-13 22:10:07 +0300

    signalr connected

---
+++ Services/SignalR/EShopper.SignalR.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EShopper.SignalR.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- Services/SignalR/EShopper.SignalR/Program.cs
+++ Services/SignalR/EShopper.SignalR/Program.cs
@@ -1,9 +1,23 @@
 using EShopper.SignalR.DataAccess.Context;
+using EShopper.SignalR.Hubs;
+using Microsoft.AspNetCore.Cors.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCors(opt =>
+{
+    var policy = new CorsPolicyBuilder()
+                    .AllowCredentials()
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .SetIsOriginAllowed(x=>true)
+                    .Build();
+    opt.AddPolicy("corsPolicy", policy);
+});
+
+builder.Services.AddSignalR();
 builder.Services.AddDbContext<SignalRContext>(opt =>
 {
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
@@ -25,7 +39,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
+app.UseCors("corsPolicy");
 app.MapControllers();
+app.MapHub<SignalRHub>("/signalrhub");
 
 app.Run();
